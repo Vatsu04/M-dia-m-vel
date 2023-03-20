64135f4a3bafb946215c4f93968c6931dca05eed
Gustavo Tascheri <gugatascheri@gmail.com>
2023-03-19 21:27:19 -0300

    Média movel

---
+++ média_móvel.py
@@ -0,0 +1,15 @@
+casos = []
+
+msg = "Com quantos numeros você gostaria de calcular a média móvel? "
+numeros = int(input(msg))
+
+while True:
+    casos.insert(0,int(input("Casos:")))
+    if len(casos) > numeros:
+        casos.pop()
+    soma = 0
+    for i in range(len(casos)):
+        soma += casos[i]
+    mm = soma/len(casos)
+
+    print(casos, round(mm,2))
